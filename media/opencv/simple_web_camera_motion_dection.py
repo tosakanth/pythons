b126a0962a464f3316f508a2a516107eaeb8ac1d
Tosakanth Project <tosakun.meeting@gmail.com>
2015-05-03 18:05:53 +0700

    Update simple_web_camera_motion_dection.py

--- media/opencv/simple_web_camera_motion_dection.py
+++ media/opencv/simple_web_camera_motion_dection.py
@@ -9,9 +9,11 @@ def diffImg(t0, t1, t2):
   # I get 2 differences then calculate difference between them again
   d3 = cv2.absdiff(d1,d2)
   
+  #I can not find the reason why cv2.mean() returns a tuple with 4 values, I want only the first element.
   #if there is not a movement (or very little movement) the mean value of result array should be low,
+  avg = cv2.mean(d3)
   
-  return cv2.mean(d3)
+  return avg[0]
 
 #initiate camera, I use camera index : 0
 cam = cv2.VideoCapture(0)
@@ -23,7 +25,8 @@ t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 threshold = 2.0 # This is my personal experience, you may calibrate it yourself.
 
 while True:
-  img_diff = diffImg(t_minus, t, t_plus)[0]
+  	
+  img_diff = diffImg(t_minus, t, t_plus)
   if img_diff > threshold :
 	  print "Movement"
   else :
