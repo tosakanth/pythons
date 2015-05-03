46af78cecf20fb80c48497d019a5151a131fb6f5
Tosakanth Project <tosakun.meeting@gmail.com>
2015-05-03 14:02:06 +0700

    Update simple_web_camera_motion_dection.py

--- media/opencv/simple_web_camera_motion_dection.py
+++ media/opencv/simple_web_camera_motion_dection.py
@@ -10,18 +10,18 @@ def diffImg(t0, t1, t2):
   # I get 2 differences then calculate difference between them again
   d3 = cv2.absdiff(d1,d2)
   
-  #if there is not a movement (or very little movement) the result value should be very low
+  #if there is not a movement (or very little movement) the result value should be low,
   #on the other hand it should be high.
   return cv2.mean(d3)
 
-#initiate camera
+#initiate camera, I use camera index : 0
 cam = cv2.VideoCapture(0)
 
 # Read first three frames:
 t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-threshold = 2.0
+threshold = 2.0 # This is my personal experience, you may calibrate it yourself.
 
 while True:
   img_diff = diffImg(t_minus, t, t_plus)[0]
